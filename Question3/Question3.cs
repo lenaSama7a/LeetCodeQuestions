106b2a0a7751a1adb7313809a52ebeb625a4f538
lenaSamaha <lena.sama7a@gmail.com>
2022-11-07 12:50:40 +0200

    Adding solution of question 3

---
+++ Question3/Question3.cs
@@ -0,0 +1,54 @@
+﻿//https://leetcode.com/problems/string-to-integer-atoi/
+//Implement the myAtoi(string s) function,
+//which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+using System;
+
+class Question3
+{
+
+    static int myAtoi(string str)
+{
+        int res = 0;
+        int sign = 1;
+        int intVal;
+
+        int i = 0;
+
+        if (str.Contains('-'))
+        {
+            sign = -1;
+            str.Remove(str.IndexOf('-'));
+            i++;
+        }
+
+        for (; i < str.Length; ++i)
+        {
+            intVal = (int)Char.GetNumericValue(str[i]); // it it's letter intVal= -1
+            if (intVal <= 9 && intVal >= 0)
+            {
+                res = res * 10 + intVal;
+            }
+            else
+            {
+                continue;
+            }
+            
+
+        }
+
+
+        return sign * res;
+    }
+
+// Driver code
+public static void Main()
+{
+    string str = "words and 987";
+
+    int val = myAtoi(str);
+    Console.Write(val);
+
+
+    }
+}
