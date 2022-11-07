60c3335e1c66dc823c39e510330736863b5b64e6
lenaSamaha <lena.sama7a@gmail.com>
2022-11-07 13:10:04 +0200

    Adding solution of Question 4

---
+++ Question4/Question4.cs
@@ -0,0 +1,44 @@
+﻿//https://leetcode.com/problems/search-insert-position/description/
+//Given a sorted array of distinct integers and a target value,
+//return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+//You must write an algorithm with O(log n) runtime complexity.
+
+using System;
+class Question4
+{
+    static int SearchInsert(int[] nums, int target)
+    {
+
+        int n = nums.Length;
+        int start = 0;
+        int end = n - 1;
+
+        // Traverse the search space
+        while (start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            // If K is found
+            if (nums[mid] == target)
+                return mid;
+
+            else if (nums[mid] < target)
+                start = mid + 1;
+
+            else
+                end = mid - 1;
+        }
+
+        // Return insert position
+        return end + 1;
+    }
+
+    // Driver Code
+    public static void Main()
+    {
+        int[] num = { 1, 3, 5, 6 };
+        int target = 5;
+
+        Console.WriteLine(SearchInsert(num, target));
+    }
+}
