ffc0d1d12a9c09ca70f9e1cf64bccf0f129daec4
lenaSamaha <lena.sama7a@gmail.com>
2022-11-11 17:29:51 +0200

    Editing solution of question 4

--- Question4/Question4.cs
+++ Question4/Question4.cs
@@ -13,12 +13,10 @@ class Question4
         int start = 0;
         int end = n - 1;
 
-        // Traverse the search space
         while (start <= end)
         {
             int mid = (start + end) / 2;
 
-            // If K is found
             if (nums[mid] == target)
                 return mid;
 
@@ -29,11 +27,9 @@ class Question4
                 end = mid - 1;
         }
 
-        // Return insert position
         return end + 1;
     }
 
-    // Driver Code
     public static void Main()
     {
         int[] num = { 1, 3, 5, 6 };
